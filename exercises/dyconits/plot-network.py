38babe6ca230f4e5b8cce05eccbe7852d89b9f82
Jesse Donkervliet <j.donkervliet@gmail.com>
2023-11-08 09:16:33 +0100

    make boxplot start from 0

--- exercises/dyconits/plot-network.py
+++ exercises/dyconits/plot-network.py
@@ -50,5 +50,6 @@ df = df.groupby(["iteration", "config"], as_index=False).max()
 df["packets_per_second"] = df["net.packets_sent.ib0"] / (df["timestamp"] / 1000)
 fig = px.box(df, x="config", y="packets_per_second", template=template,
              labels={"config": "Dyconit policy", "packets_per_second": "packets per second"})
+fig.update_yaxes(rangemode="tozero")
 with open(str(output_dir.joinpath("packets_per_second_over_policy.pdf")), "wb") as f:
     f.write(scope.transform(fig, format="pdf"))
